7721a1bf4e055495f4c60c856f8f977cda212fcd
ModischFabrications <modisch.fabrications@gmail.com>
2022-05-30 23:43:44 +0200

    add handtrackjs and display it's predictions; still mayor wip

--- src/components/Preview.svelte
+++ src/components/Preview.svelte
@@ -1,13 +1,79 @@
 <script lang="ts">
+    import * as handTrack from "handtrackjs";
+
+    const modelParams = {
+        flipHorizontal: true, // flip e.g for video
+        maxNumBoxes: 10, // maximum number of boxes to detect
+        iouThreshold: 0.5, // ioU threshold for non-max suppression
+        scoreThreshold: 0.3, // confidence threshold for predictions.
+    };
+
+    let canvas: HTMLCanvasElement;
+    let video: HTMLVideoElement;
+    let isVideo: boolean;
+
+    let model = null;
+
+    let modelPromise = handTrack.load(modelParams).then((lmodel) => {
+        model = lmodel;
+        console.log("model loaded");
+
+        // TODO connect to toggle/dropdown
+        setTimeout(() => {
+            startVideo();
+        }, 2000);
+    });
+
+    function runDetection() {
+        model.detect(video).then((predictions) => {
+            if (!isVideo) return;
+
+            model.renderPredictions(
+                predictions,
+                canvas,
+                canvas.getContext("2d"),
+                video
+            );
+
+            // queue for next frame
+            requestAnimationFrame(runDetection);
+        });
+    }
+
+    function startVideo() {
+        console.log("Starting video stream...");
+        // video.width and .height are possible, but not needed atm
+
+        // TODO add promise to second #await
+        handTrack.startVideo(video).then(function (res) {
+            console.log("--> " + res.msg);
+            if (res.status) {
+                console.log("Video started. Now tracking");
+                isVideo = true;
+                runDetection();
+            } else {
+                console.error("Please enable video");
+            }
+            video.style.height = "";
+        });
+    }
+
+    function stopVideo() {
+        handTrack.stopVideo(video);
+        isVideo = false;
+    }
 </script>
 
 <div class="preview-container" alt="video stream preview">
-    <p>This will be a video soon...</p>
-    <!-- TODO {#await tracking}
-        
-    {:then } 
-        
-    {/await}-->
+    {#await modelPromise}
+        <p>The AI is still waking up, this might take a while..</p>
+    {:then res}
+        <!-- svelte-ignore a11y-media-has-caption -->
+        <video id="video" style="display: none;" bind:this={video} />
+        <canvas id="canvas" bind:this={canvas} />
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
 </div>
 
 <style>
